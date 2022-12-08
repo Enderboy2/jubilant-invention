0c87587b6bfad315fe6778a258ec390eebb18f8a
YoussefKarim <y01006452923@gmail.com>
2022-12-08 12:02:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
         <Title>Markdown Editor</Title>
         <EditorContainer>
           <MarkedInput />
-          <Result />
+          <Result / >
         </EditorContainer>
       </AppContainer>
     </EditorContext.Provider>
